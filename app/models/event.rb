af72eb34f7bfcee911d5626abc4b11a4e715d1eb
luciavzqz <luciamaitev@gmail.com>
2019-07-19 17:18:31 -0300

    Validaciones de datos para la creación de eventos

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,8 @@
 class Event < ApplicationRecord
-  
+	validates :name, :place, :start_hour, :description, :event_date, :max_capacity, :speakers, presence: true
+	validates :speakers, format: { with: /\A[a-zA-Z]+\z/}
+	validates :name, length: { maximum: 30 }
+	validates :place, length: { maximum: 30 }
+	validates :name, uniqueness: true
+	validates :description, length: { in: 50..300 }
 end

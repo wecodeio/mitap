4b3eec765d28572dab52c29cfeb4c41121b0ffe5
luciavzqz <luciamaitev@gmail.com>
2019-07-21 15:25:10 -0300

    Comentarios e intento de validar_hora
    Comentario en scaffolds.scss, e intento de función de validar_hora en el model de events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,17 @@
 class Event < ApplicationRecord
-	validates :name, :place, :start_hour, :description, :event_date, :max_capacity, :speakers, presence: true
+=begin	validates :name, :place, :start_hour, :end_hour, :description, :event_date, :max_capacity, :speakers, presence: true
 	validates :speakers, format: { with: /\A[a-zA-Z]+\z/}
 	validates :name, length: { maximum: 30 }
 	validates :place, length: { maximum: 30 }
 	validates :name, uniqueness: true
 	validates :description, length: { in: 50..300 }
+=end
+	validate :validar_hora
+
+	def validar_hora
+		if start_hour > end_hour
+			errors.add(start_hour, "Hora de inicio inválida")
+			errors.add(end_hour, "Hora de fin inválida")
+		end
+	end
 end

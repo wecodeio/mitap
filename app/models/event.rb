8b9c6833dd717133f51f04fdb320166de750eeb9
luciavzqz <luciamaitev@gmail.com>
2019-07-25 11:15:44 -0300

    New design editEvent:admin

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,16 +1,16 @@
 class Event < ApplicationRecord
 	
-	validates :name, :place, :start_hour, :end_hour, :description, :event_date, :max_capacity, :speakers, presence: true
-	validates :speakers, format: { with: /\A[a-zA-Z]+\z/}
-	validates :name, length: { maximum: 30 }
-	validates :place, length: { maximum: 30 }
-	validates :name, uniqueness: true
-	validates :description, length: { in: 50..300 }
+	# validates :name, :place, :start_hour, :end_hour, :description, :event_date, :max_capacity, :speakers, presence: true
+	# validates :speakers, format: { with: /\A[a-zA-Z]+\z/}
+	# validates :name, length: { maximum: 30 }
+	# validates :place, length: { maximum: 30 }
+	# validates :name, uniqueness: true
+	# validates :description, length: { in: 50..300 }
 
-	validates :max_capacity, numericality: { only_integer: true }
+	# validates :max_capacity, numericality: { only_integer: true }
 
-	validate :validar_fecha
-	validate :validar_hora
+	# validate :validar_fecha
+	# validate :validar_hora
 
 	def validar_hora
 		if start_hour.present?and

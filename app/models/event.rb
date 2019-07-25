83c7dcc9c20d8da3c9638bf796ca13b78b77c2eb
luciavzqz <luciamaitev@gmail.com>
2019-07-25 00:58:18 -0300

    Actualización de validaciones
    Se quitan comentarios y se agrega validación a la fecha

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,15 +1,16 @@
 class Event < ApplicationRecord
-=begin	validates :name, :place, :start_hour, :end_hour, :description, :event_date, :max_capacity, :speakers, presence: true
+	
+	validates :name, :place, :start_hour, :end_hour, :description, :event_date, :max_capacity, :speakers, presence: true
 	validates :speakers, format: { with: /\A[a-zA-Z]+\z/}
 	validates :name, length: { maximum: 30 }
 	validates :place, length: { maximum: 30 }
 	validates :name, uniqueness: true
 	validates :description, length: { in: 50..300 }
-=end
-	#validates :nombre, presence: true
+	validates :nombre, presence: true
 
-	#validates :max_capacity, numericality: { only_integer: true }
+	validates :max_capacity, numericality: { only_integer: true }
 
+	validate :validar_fecha
 	validate :validar_hora
 
 	def validar_hora
@@ -18,6 +19,12 @@ class Event < ApplicationRecord
 			start_hour > end_hour
 			errors.add(:start_hour, "Hora de inicio inválida")
 			errors.add(:end_hour, "Hora de fin inválida")
+		end
+	end
+
+	def validar_fecha
+		if event_date < Time.now
+			errors.add(:event_date, "Fecha inválida")
 		end
 	end
 end

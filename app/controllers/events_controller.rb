8bd234543c41f8d835f123503adea301e4c6f9a4
Ariel <arielgutierrez2001@gmail.com>
2019-08-05 15:30:20 -0300

    Validaciones para una inscripción funcionando. Que el usuario esté logueado, que el cupo lo permita, que el evento exista y que el usuario no se haya inscripto anteriormente al mismo.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,13 +5,40 @@ class EventsController < ApplicationController
   end
 
   def show
-  	@event_group = EventGroup.find(params[:id])
   	@event = Event.find(params[:id])
   end
 
   def register
-  		Registration.create(event_id: params[:id], user_id: session[:user_id])
-  		redirect_to event_path, notice: "te inscribiste correctamente al evento"
+  	user = current_user
+    event = Event.find(params[:id])
+
+  	if user == nil 
+      redirect_to event_path
+      flash.alert = "Para inscribirte debes estar logueado"
+      return
+    end
+
+    if event == nil
+      redirect_to event_path
+      flash.alert = "El evento no existe"
+      return
+    end
+
+    if event.max_capacity <= event.registrations.count
+      redirect_to event_path
+      flash.alert = "El evento está completo"
+      return
+    end
+
+    if Registration.where(user: user, event: event).exists?
+  		redirect_to event_path
+      flash.alert = "Ya está inscripto a este evento"
+      return
+  	end
+
+    Registration.create(event_id: params[:id], user_id: session[:user_id])
+    redirect_to event_path
+    flash.alert = "te inscribiste correctamente al evento"
   end
   		
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,5 @@
 class SessionsController < ApplicationController
-  	
-
+  
   	def new
 	end
 
@@ -8,7 +7,8 @@ class SessionsController < ApplicationController
 	  user = User.find_by_email(params[:email])
 	  if user && user.authenticate(params[:password])
 	    session[:user_id] = user.id
-	    redirect_to events_path, notice: "Sesión iniciada!"
+	    redirect_to events_path
+	    flash.alert = "Sesión iniciada!"
 	  else
 	    flash.now.alert = "Email o password es inválida"
 	    render "new"
@@ -17,6 +17,7 @@ class SessionsController < ApplicationController
 
 	def destroy
 	  session[:user_id] = nil
-	  redirect_to events_path, notice: "Sesión cerrada!"
+	  redirect_to events_path
+	  flash.alert = "Sesión cerrada!"
 	end
 end
